6541319bc9e4ca0e4dc3a5c1e733f83368d28835
DayeonJung <ekdus0227@gmail.com>
2022-04-26 11:01:08 +0900

    feat: 호출 예시 코드 생성

--- Example/DYWave/ViewController.swift
+++ Example/DYWave/ViewController.swift
@@ -7,12 +7,26 @@
 //
 
 import UIKit
+import DYWave
 
 class ViewController: UIViewController {
-
+    var waveView: WaveView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        self.waveView = WaveView(
+            frame: CGRect(
+                x: 0,
+                y: 0,
+                width: UIScreen.main.bounds.width,
+                height: UIScreen.main.bounds.height
+            ),
+            bgColor: .lightGray,
+            maxTime: 3
+        )
+        
+        self.view.insertSubview(self.waveView, at: 0)
     }
 
     override func didReceiveMemoryWarning() {
